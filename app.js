4205b1a31d887d20b83fec65d22f51030535d0f3
marilinasc <marilina.scandalo@gmail.com>
2022-09-04 22:07:53 -0300

    arrays

--- app.js
+++ app.js
@@ -3,60 +3,64 @@ function nombreCliente () {
     alert (nombre + ", te mostraremos diferentes modelos de Rollers")
 }
 
-function importeCuotas () {
-    switch (producto) {
-        case "1":
-            return (fusion/3)
-        case "2":
-            return (street/3)
-        case "3":
-            return (streetDark/3)
+// Variables
+let nombre = prompt ("Bienvenido/a a Go Roll, la tienda online de Rollers. Ingrese su nombre:")
+let entrada = 0 
+
+// Objetos
+class Producto  {
+    constructor (nombre,precio) {
+    this.nombre = nombre
+    this.precio = precio
+    }
+    descuento () {
+        alert ("Tienes un 15% off, el precio final del modelo " +this.nombre +" es $" +(this.precio -(this.precio*0.15)))
+    }
+    cuotas (){
+        alert ("El precio de cada cuota del modelo " +this.nombre +" es $" +(this.precio /3))
     }
 }
 
-function descuento () {
-    switch (producto) {
-        case "1":
-            return (fusion - (fusion*0.15))
-        case "2":
-            return (street - (street*0.15))
-        case "3":
-            return (streetDark - (streetDark*0.15))
-    }
-}
-// Variables
-let nombre = prompt ("Bienvenido/a a Go Roll, la tienda online de Rollers. Ingrese su nombre:")
-let fusion = 32000
-let street = 28000
-let streetDark = 35000
+//Arrays
+const carrito = []
 
 // Main
 nombreCliente ()
-let producto = prompt ("¿Que producto desea comprar? Indique una opcion: \n1: FUSION \n2: STREET \n3: STREET DARK")
-switch (producto) {
-    case "1":
-        alert ("Seleccionaste el modelo FUSION, el precio es $" +fusion)
-        break;
-    case "2":
-        alert ("Seleccionaste el modelo STREET, el precio es $" +street)
-        break;
-    case "3":
-        alert ("Seleccionaste el modelo STREET DARK, el precio es $"+streetDark)
-        break;
-    default:
-        alert ("No ingreso una opcion correcta")
-        break
+entrada = prompt ("¿Que producto desea comprar? Indique una opcion: \n1: FUSION \n2: STREET \n3: STREET DARK \n4: Finalizar")
+
+while (entrada !=4) {
+    switch (entrada){
+        case "1":
+            carrito.push (new Producto ("FUSION",32000))
+            alert ("Producto añadido al carrito")
+            break;
+        case "2":
+            carrito.push (new Producto ("STREET",28000))
+            alert ("Producto añadido al carrito")
+            break;
+        case "3":
+            carrito.push (new Producto ("STREET DARK",35000))
+            alert ("Producto añadido al carrito")
+            break;
+    }
+    entrada = prompt ("¿Desea comprar otro producto? Indique una opcion: \n1: FUSION \n2: STREET \n3: STREET DARK \n4: Finalizar")    
 }
+
+for (const producto of carrito) {
+    alert ("Seleccionaste el modelo " +producto.nombre +", el precio es $" +producto.precio)
+}
+alert ("Total de productos pendientes para comprar: " +carrito.length)
+
 
 let formaPago = prompt ("Seleccione la forma de pago: \n1: Tarjeta de credito (3 cuotas) \n2: Efectivo")
 if (formaPago == "1") {
-    for (let i=1; i <= 3; i++) {
-        alert (`El importe de la cuota ${i} es $` +importeCuotas())
+    for (const producto of carrito) {
+        producto.cuotas ()
     }
 } else if(formaPago == "2") {
-        alert ("Tienes un descuento del 15% por pago en Efectivo, el precio final es $" +descuento ())
-} else {
-        alert ("No ingreso una opcion correcta")
+    for (const producto of carrito) {
+        producto.descuento ()
+    }
 }
 
 alert ("¡Gracias por su compra!")
